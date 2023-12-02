f700f405564132ac15b0a8410577dea6a6ace497
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-02 12:14:22 -0500

    Solve exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof num1 !== 'number' || typeof num2 !== 'number' ) {
+        return 'ERROR';
+    } else if (num1 < 0 || num2 < 0) {
+        return 'ERROR';
+    } else {
+        for (i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line
