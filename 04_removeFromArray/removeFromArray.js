c201c4736bb55157557f93cb7a7a20a2776d8a6a
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-02 11:56:33 -0500

    Solve exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        for (i = 0; i < arr.length; i++) {
+            if (arg === arr[i]) {
+                arr.splice(i, 1);
+            }
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
