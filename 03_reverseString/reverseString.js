d800ae0720c411910680a75a99976a8482c3c113
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-02 11:08:38 -0500

    Solve exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let letters = string.split('');
+    let reversedArray = [];
+    for (i = letters.length; i >= 0; i--) {
+        reversedArray.push(letters[i]);
+    }
+    return reversedArray.join('');
 };
 
 // Do not edit below this line
