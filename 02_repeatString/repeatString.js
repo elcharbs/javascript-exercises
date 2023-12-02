29a386e9f0ed4e58e0ab683c9ee954bff5ed1a19
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-02 11:00:18 -0500

    Finish exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+function repeatString (string, num) {
+    outputString = [];
+    if (num < 0) {return 'ERROR'} else {
+    for (i=0; i<num; i++) {
+        outputString.push(string);
+    }
+    return outputString.join("");
+}
 };
 
 // Do not edit below this line
