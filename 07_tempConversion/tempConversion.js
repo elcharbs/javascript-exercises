97c0210fc154aebd588e7a5f0f6e19a9178194d6
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-02 12:38:21 -0500

    Solve exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempInF) {
+  convertedTemp = (tempInF - 32) * (5/9);
+  if (convertedTemp !== Math.floor(convertedTemp)) {
+    return Number(convertedTemp.toFixed(1));
+  }
+  return convertedTemp;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempInC) {
+  convertedTemp = (tempInC * (9/5)) + 32;
+  if (convertedTemp !== Math.floor(convertedTemp)) {
+    return Number(convertedTemp.toFixed(1));
+  }
+  return convertedTemp;
 };
 
 // Do not edit below this line
