7b48a165694920a11ac529b131868292394ecd18
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-24 13:22:48 -0500

    Solve palindromes exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    string = string
+    .replace(/[^\w\s\']|_/g,'')
+    .replace(/\s+/g,"")
+    .toLowerCase();
+  stringArray = string.split('');
+  reversedArray = string.split('').reverse();
+  return stringArray.length === reversedArray.length && stringArray.every((value, index) => value === reversedArray[index]);
 };
 
 // Do not edit below this line
