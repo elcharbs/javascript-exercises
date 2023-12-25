9bbb841590cf59bab671ba21a53cd7ea477bc4e9
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-25 14:19:36 -0500

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,15 @@
-const findTheOldest = function() {
-
-};
+const findTheOldest = function(people) {
+    var currentTime = new Date();
+    people.forEach(element => {
+      if (typeof element.yearOfDeath === 'undefined') {
+        element.age = currentTime.getFullYear() - element.yearOfBirth;
+      } else {
+      element.age =  element.yearOfDeath - element.yearOfBirth;
+      }
+    });
+    people.sort((a, b) => b.age - a.age);
+    return people[0];
+  };
 
 // Do not edit below this line
 module.exports = findTheOldest;
