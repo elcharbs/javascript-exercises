8d72ba5c17debbf7504875008184d8ad8ce1dbf7
elcharbs <60406297+elcharbs@users.noreply.github.com>
2023-12-25 13:12:50 -0500

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function (num) {
+    let sequence = [1, 1];
+    let n = 1;
+    if (num <= 0) {
+      return "OOPS";
+    }
+    if (num <= 2) {
+      return sequence[(num-1)];
+    }
+    while (sequence.length < num) {
+      let newValue = sequence[n] + sequence[n-1];
+      sequence.push(newValue);
+      n++;
+    } 
+    return sequence[num - 1];
+  };
 
 // Do not edit below this line
 module.exports = fibonacci;
